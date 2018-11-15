f98cecaa3b1c88f1ad1a00e2f3812d2e65029f33
santisauge <45068024+santisauge@users.noreply.github.com>
2018-11-15 09:30:34 -0300

    Add files via upload

--- ejercicio1/main.c
+++ ejercicio1/main.c
@@ -10,30 +10,36 @@ struct alumno_t {
 
 int main(int argc, char* argv[])
 {
-	int cant_alumnos, i;
-	struct alumno_t alumnos[MAX_ALUMNOS];
-	
+   int cant_alumnos, i;
+    struct alumno_t alumnos[MAX_ALUMNOS];
+    FILE *sauge;
+    fopen("salida.out","+w");
+    if(sauge){
+        printf("el archivo no se pudo abrir");
+    }
+
 	printf("Cuantos alumnos va a ingresar? (Maximo %d): ", MAX_ALUMNOS);
 	scanf("%d", &cant_alumnos);
 
 	if(cant_alumnos > MAX_ALUMNOS) {
-		fprintf(stderr, "Error: Cantidad de alumnos no v√°lida");
+		fprintf(stderr, "Error: Cantidad de alumnos no v·lida");
 		return -1;
 	}
 
 	for(i=0 ; i < cant_alumnos ; i++) {
 		printf("\n\n\n === Alumno %d de %d === \n", i, cant_alumnos);
-		printf("\nIngrese el DNI: ");	
+		printf("\nIngrese el DNI: ");
 		scanf("%d", &alumnos[i].dni);
-		printf("\nIngrese el Promedio: ");	
+		printf("\nIngrese el Promedio: ");
 		scanf("%d", &alumnos[i].promedio);
 	}
 
 	printf("\n\n === Fin del ingreso, guardando datos ... ===");
 
 
-	//AGREGE A CONTINUACI√ìN SU C√ìDIGO
+	//AGREGE A CONTINUACI”N SU C”DIGO
 	//EL ARCHIVO DE SALIDA DEBE LLAMARSE 'salida.out'
-
+    fwrite(alumnos,sizeof(struct alumno_t),cant_alumnos,sauge);
+    fclose(sauge);
 	return 0;
 }
