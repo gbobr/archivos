4a14679143bfe2db35b8013ebafb923895bde082
Portaliekx <44263591+Portaliekx@users.noreply.github.com>
2018-11-15 09:16:32 -0300

    Add files via upload

--- ejercicio1/main.c
+++ ejercicio1/main.c
@@ -10,9 +10,14 @@ struct alumno_t {
 
 int main(int argc, char* argv[])
 {
+    FILE *f;
 	int cant_alumnos, i;
 	struct alumno_t alumnos[MAX_ALUMNOS];
-	
+	f =fopen("salidad.out", "+w");
+	if(f){
+        printf("ERROR en archivo no pudo abrir/cerrar");
+	}
+
 	printf("Cuantos alumnos va a ingresar? (Maximo %d): ", MAX_ALUMNOS);
 	scanf("%d", &cant_alumnos);
 
@@ -23,9 +28,9 @@ int main(int argc, char* argv[])
 
 	for(i=0 ; i < cant_alumnos ; i++) {
 		printf("\n\n\n === Alumno %d de %d === \n", i, cant_alumnos);
-		printf("\nIngrese el DNI: ");	
+		printf("\nIngrese el DNI: ");
 		scanf("%d", &alumnos[i].dni);
-		printf("\nIngrese el Promedio: ");	
+		printf("\nIngrese el Promedio: ");
 		scanf("%d", &alumnos[i].promedio);
 	}
 
@@ -34,6 +39,7 @@ int main(int argc, char* argv[])
 
 	//AGREGE A CONTINUACIÓN SU CÓDIGO
 	//EL ARCHIVO DE SALIDA DEBE LLAMARSE 'salida.out'
-
-	return 0;
+    fwrite(alumnos,sizeof(struct alumno_t),cant_alumnos,f);
+    fclose(f);
+    return 0;
 }
