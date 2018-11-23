211e65be241aac660176e4d992f7ff66d4ede442
Matias Garcia <43641235+matingb@users.noreply.github.com>
2018-11-23 15:34:31 -0300

    Add files via upload

--- ejercicio1/main.c
+++ ejercicio1/main.c
@@ -12,27 +12,35 @@ int main(int argc, char* argv[])
 {
 	int cant_alumnos, i;
 	struct alumno_t alumnos[MAX_ALUMNOS];
-	
+	FILE* f;
+	f = fopen("salida.out", "w");
+	if(f){
+        printf("Error");
+	}
+
 	printf("Cuantos alumnos va a ingresar? (Maximo %d): ", MAX_ALUMNOS);
 	scanf("%d", &cant_alumnos);
 
 	if(cant_alumnos > MAX_ALUMNOS) {
-		fprintf(stderr, "Error: Cantidad de alumnos no v√°lida");
+		fprintf(stderr, "Error: Cantidad de alumnos no v·lida");
 		return -1;
 	}
 
 	for(i=0 ; i < cant_alumnos ; i++) {
 		printf("\n\n\n === Alumno %d de %d === \n", i, cant_alumnos);
-		printf("\nIngrese el DNI: ");	
+		printf("\nIngrese el DNI: ");
 		scanf("%d", &alumnos[i].dni);
-		printf("\nIngrese el Promedio: ");	
+		printf("\nIngrese el Promedio: ");
 		scanf("%d", &alumnos[i].promedio);
 	}
 
 	printf("\n\n === Fin del ingreso, guardando datos ... ===");
 
 
-	//AGREGE A CONTINUACI√ìN SU C√ìDIGO
+	//AGREGE A CONTINUACI”N SU C”DIGO
+	fwrite(alumnos, sizeof(struct alumno_t), cant_alumnos, f);
+	fclose(f);
+
 	//EL ARCHIVO DE SALIDA DEBE LLAMARSE 'salida.out'
 
 	return 0;
