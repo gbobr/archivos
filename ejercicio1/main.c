e7fe6e34fd0684151ce828ee23408583b697173c
emilianosce <45399630+emilianosce@users.noreply.github.com>
2018-11-27 14:06:43 -0300

    Ejercicio Archivos 1 Scevola

--- ejercicio1/main.c
+++ ejercicio1/main.c
@@ -1,39 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-#define MAX_ALUMNOS 50
-
-struct alumno_t {
+ #define MAX_ALUMNOS 50
+ struct alumno_t {
 	int dni;
 	int promedio;
 };
-
-int main(int argc, char* argv[])
+ int main(int argc, char* argv[])
 {
 	int cant_alumnos, i;
 	struct alumno_t alumnos[MAX_ALUMNOS];
-	
-	printf("Cuantos alumnos va a ingresar? (Maximo %d): ", MAX_ALUMNOS);
+	FILE* f;
+	f = fopen("salida.out", "w");
+	if(f){
+        printf("Error");
+	}
+ 	printf("Cuantos alumnos va a ingresar? (Maximo %d): ", MAX_ALUMNOS);
 	scanf("%d", &cant_alumnos);
-
-	if(cant_alumnos > MAX_ALUMNOS) {
-		fprintf(stderr, "Error: Cantidad de alumnos no vÃ¡lida");
+ 	if(cant_alumnos > MAX_ALUMNOS) {
+		fprintf(stderr, "Error: Cantidad de alumnos no válida");
 		return -1;
 	}
-
-	for(i=0 ; i < cant_alumnos ; i++) {
+ 	for(i=0 ; i < cant_alumnos ; i++) {
 		printf("\n\n\n === Alumno %d de %d === \n", i, cant_alumnos);
-		printf("\nIngrese el DNI: ");	
+		printf("\nIngrese el DNI: ");
 		scanf("%d", &alumnos[i].dni);
-		printf("\nIngrese el Promedio: ");	
+		printf("\nIngrese el Promedio: ");
 		scanf("%d", &alumnos[i].promedio);
 	}
-
-	printf("\n\n === Fin del ingreso, guardando datos ... ===");
-
-
-	//AGREGE A CONTINUACIÃ“N SU CÃ“DIGO
-	//EL ARCHIVO DE SALIDA DEBE LLAMARSE 'salida.out'
-
-	return 0;
+ 	printf("\n\n === Fin del ingreso, guardando datos ... ===");
+ 	//AGREGE A CONTINUACIÓN SU CÓDIGO
+	fwrite(alumnos, sizeof(struct alumno_t), cant_alumnos, f);
+	fclose(f);
+ 	//EL ARCHIVO DE SALIDA DEBE LLAMARSE 'salida.out'
+ 	return 0;
 }
